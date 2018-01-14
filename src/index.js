e6fbd714057fd2e3a2a18b4f94fef2502e00d7d3
gunheekim90 <gunhee21@gmail.com>
2018-01-15 00:57:46 +0900

    Add axios default setting value

--- src/index.js
+++ src/index.js
@@ -10,7 +10,22 @@ import RootRoute from './Router/index';
 import './index.css';
 
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
+import querystring from 'querystring';
+//////////////////////////////////////////////////////////////////////////////////
+// AJAX Setting                                                                 //
+//////////////////////////////////////////////////////////////////////////////////
+//axios.defaults.baseURL = '';
+axios.defaults.withCredentials = true;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+axios.defaults.headers.put['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+axios.defaults.headers.patch['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
+
+axios.defaults.transformRequest = [function (data) {
+    if (data instanceof FormData) return data;
+    return querystring.stringify(data);
+}];
 
 const store = createStore(
   rootReducer,
