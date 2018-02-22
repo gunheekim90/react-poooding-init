3e4deda51031db5b482faf6a72eff6e558da794b
root <root@ip-172-31-12-195.ap-northeast-2.compute.internal>
2018-02-22 15:44:21 +0000

    a

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  	console.log("들어왔따");
+	res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(80,function(){
